02ebce8e093dc2a0277e8db91af34557af9c080b
mohan1733 <mohan960077@gmail.com>
2026-02-11 19:49:45 +0530

    update

---
+++ core-modules/syn.js
@@ -0,0 +1,31 @@
+const p = require("fs");
+
+
+// write file (synchronous)
+// p.writeFileSync('text.txt','hello node js');
+// p.writeFileSync('../file/text.txt','hello node js');
+// console.log('file written');
+
+// read file
+// const data=p.readFileSync("text.txt",{encoding: 'utf-8' })
+// console.log("read:",data)
+
+// append file
+// p.appendFileSync("text.txt","\nthis is appended text ,hello world");
+// console.log("text update")
+
+// delete file
+// p.unlinkSync('text.txt');
+// console.log("file deleted")
+
+// create folder
+// p.mkdirSync("folder");
+// console.log("folder created")
+
+// delete folder
+// p.rmdirSync("folder");
+// console.log("folder")
+
+// exists file
+// p.existsSync("text.txt");
+// console.log("file exists")
