756c15a21ccebd668219e0a91a92b96f67bf4bbc
mohan1733 <mohan960077@gmail.com>
2026-02-12 19:17:06 +0530

    update

---
+++ core-modules/path.js
@@ -0,0 +1,13 @@
+const p=require('path')
+
+const filePath="user/folder/project/file.txt";
+
+
+console.log("base name:",p.basename(filePath));
+console.log("directory name:",p.dirname(filePath));
+console.log("extension name:",p.extname(filePath));
+console.log("current file:",__dirname);
+console.log("file name:",__filename);
+console.log(p.resolve("text.txt"))
+console.log("joined path:",p.join(__dirname,"file","text.txt"))
+
