1ee1aefe90f826f09a473cf8389d26617e7f497d
mohan1733 <mohan960077@gmail.com>
2026-02-13 19:21:19 +0530

    update

--- core-modules/os.js
+++ core-modules/os.js
@@ -1,12 +1,11 @@
-const p=require('os');
+const p = require("os");
 
-console.log("userinfo:",p.userInfo())
-console.log("platform:",p.platform())
-console.log("host name:",p.hostname())
-console.log("architecture:",p.arch())
-console.log("home directory:",p.homedir())
-console.log("cpu cores:",p.cpus().length)
-console.log("os uptime:",p.uptime())
-console.log("total memory:",Math.round(p.totalmem()/1024**3),'GB');
-console.log("free memory:",Math.round(p.freemem()/1024**3),'GB');
-
+console.log("userinfo:", p.userInfo());
+console.log("platform:", p.platform());
+console.log("host name:", p.hostname());
+console.log("architecture:", p.arch());
+console.log("home directory:", p.homedir());
+console.log("cpu cores:", p.cpus().length);
+console.log("os uptime:", p.uptime());
+console.log("total memory:", Math.round(p.totalmem() / 1024 ** 3), "GB");
+console.log("free memory:", Math.round(p.freemem() / 1024 ** 3), "GB");
--- core-modules/path.js
+++ core-modules/path.js
@@ -1,13 +1,11 @@
-const p=require('path')
+const p = require("path");
 
-const filePath="user/folder/project/file.txt";
+const filePath = "user/folder/project/file.txt";
 
-
-console.log("base name:",p.basename(filePath));
-console.log("directory name:",p.dirname(filePath));
-console.log("extension name:",p.extname(filePath));
-console.log("current file:",__dirname);
-console.log("file name:",__filename);
-console.log(p.resolve("text.txt"))
-console.log("joined path:",p.join(__dirname,"file","text.txt"))
-
+console.log("base name:", p.basename(filePath));
+console.log("directory name:", p.dirname(filePath));
+console.log("extension name:", p.extname(filePath));
+console.log("current file:", __dirname);
+console.log("file name:", __filename);
+console.log(p.resolve("text.txt"));
+console.log("joined path:", p.join(__dirname, "file", "text.txt"));
